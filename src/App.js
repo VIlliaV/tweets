8a8c7e534df00869e6ff514062d1422b24e092e1
Illia <illia.vasiliev@gmail.com>
2023-05-10 19:10:38 +0300

    fix comment navigate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from './components/Layout/Layout';
 import { lazy } from 'react';
