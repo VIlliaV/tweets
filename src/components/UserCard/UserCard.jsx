0aa4da9ccf619744c55a70940a26fc556ad28305
Illia <illia.vasiliev@gmail.com>
2023-05-15 16:20:05 +0300

    just local for UserCard

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,37 +1,25 @@
 import noAvatar from 'assets/noAvatar.png';
 import { Card } from './UsersCard.styled';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from 'components/Buttons/Button/Button';
 import { getLocalFollow, setLocalFollow } from 'services/Local/local';
 
 export const UserCard = ({ userInfo }) => {
   const [follow, setFollow] = useState(false);
-  const [followData, setFollowData] = useState([]);
-  const firstRender = useRef(true);
 
   const { id, avatar, followers, tweets } = userInfo;
 
   useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      setFollowData(getLocalFollow() || []);
-    } else {
-      setLocalFollow(followData);
-    }
-  }, [followData]);
-
-  useEffect(() => {
-    const isFollowNow = followData.some(obj => obj.id === id);
+    const isFollowNow = getLocalFollow().some(obj => obj.id === id);
     isFollowNow ? setFollow(true) : setFollow(false);
-  }, [followData, id]);
+  }, [id]);
 
   const handleFollow = () => {
     setFollow(!follow);
     if (follow === false) {
-      setFollowData([...getLocalFollow(), { ...userInfo, isFollow: true }]);
+      setLocalFollow([...getLocalFollow(), { ...userInfo, isFollow: true }]);
     } else {
-      const deleteFollow = getLocalFollow().filter(obj => obj.id !== id);
-      setFollowData(deleteFollow);
+      setLocalFollow(getLocalFollow().filter(obj => obj.id !== id));
     }
   };
 
